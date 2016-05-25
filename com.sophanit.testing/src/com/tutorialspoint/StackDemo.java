6ab1e2f98c801a332b6ddc7943151f505aedb99a
kongnara-sophanit <kongnara2303@gmail.com>
2016-05-25 14:59:31 +0700

    Testing

---
+++ com.sophanit.testing/src/com/tutorialspoint/StackDemo.java
@@ -0,0 +1,30 @@
+package com.tutorialspoint;
+
+import java.util.*;
+
+public class StackDemo {
+	public static void main(String args[]) {
+		// creating stack
+		Stack st = new Stack();
+
+		// populating stack
+		st.push("Kongao");
+		st.push("Source");
+		st.push("code");
+		st.push("last");
+		st.push("Kongao");
+		st.push("Source");
+		st.push("code");
+		st.push("last");
+
+		// checking the top object
+		System.out.println("Top object is: " + st.peek());
+
+		// removing top object
+		System.out.println("Removed object is: " + st.pop());
+
+		// elements after remove
+		System.out.println("Elements after remove: " + st);
+
+	}
+}
